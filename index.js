5002c1db04c1fe6c209c57601718e5b1ecfeb94e
AnaBykova <anabykovaca@gmail.com>
2023-07-10 18:20:57 -0600

    turn the 2 task functions into task object methods

--- index.js
+++ index.js
@@ -31,9 +31,6 @@ logTaskState(0); // Clean Cat Litter has not been completed
 completeTask(0);
 logTaskState(0); // Clean Cat Litter has been completed
 
-*/
-
-
 function newTask(title, description) {
   const task = {
     title: title,
@@ -52,6 +49,24 @@ function logTaskState(task) {
 function completeTask(task) {
   task.complete = true;
 }
+*/
+
+function newTask(title, description) {
+  const task = {
+    title: title,
+    description: description,
+    complete: false,
+
+    logState: function() {
+      console.log(`${this.title} has${this.complete ? " " : " not "}been completed`);
+    },
+
+    markCompleted: function() {
+      this.complete = true;
+    }
+  };
+  return task;
+}
 
 // DRIVER CODE BELOW
 
@@ -59,9 +74,6 @@ const task1 = newTask("Clean Cat Litter", "Take all the 💩 out of the litter b
 const task2 = newTask("Do Laundry", "😨");
 const tasks = [task1, task2];
 
-logTaskState(task1); // Clean Cat Litter has not been completed
-completeTask(task1);
-logTaskState(task1); // Clean Cat Litter has been completed
-
-console.log(tasks);
-
+task1.logState(); // Clean Cat Litter has not been completed
+task1.markCompleted();
+task1.logState(); // Clean Cat Litter has been completed
