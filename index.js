3f178d913e63301663db4f6942e2a67af1f28414
AnaBykova <anabykovaca@gmail.com>
2023-07-10 18:16:35 -0600

    object + function

--- index.js
+++ index.js
@@ -43,12 +43,25 @@ function newTask(title, description) {
   return task;
 }
 
+// prints out the provided task's details
+function logTaskState(task) {
+  console.log(`${task.title} has${task.complete ? " " : " not "}been completed`);
+}
+
+// marks the provided task as completed
+function completeTask(task) {
+  task.complete = true;
+}
+
 // DRIVER CODE BELOW
 
 const task1 = newTask("Clean Cat Litter", "Take all the 💩 out of the litter box");
 const task2 = newTask("Do Laundry", "😨");
 const tasks = [task1, task2];
 
-// for now, let's just make sure we see our tasks
+logTaskState(task1); // Clean Cat Litter has not been completed
+completeTask(task1);
+logTaskState(task1); // Clean Cat Litter has been completed
+
 console.log(tasks);
 
